cf73b61830abee3a6f267e71e4330e35dda39aee
Jarek Lipski <loomchild@loomchild.net>
2017-05-15 00:36:09 +0200

    Fix secret generation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,21 +3,8 @@ set -e
 
 case "$*" in
     *rails*server*)
-        if [ ! -e /mastodon/private/migrated-1 ]; then
-            touch /mastodon/private/migrated-1
-            echo "initializing/updating Mastodon"
-            sleep 5
-            
-            VERSION_BAK=$VERSION
-            unset VERSION
-            rake db:migrate
-            export VERSION=$VERSION_BAK
-            
+        if [ ! -e /mastodon/private/secret ]; then
             rake secret > /mastodon/private/secret
-            
-            rake assets:precompile
-
-            rails runner /mastodon/create_admin.rb
         fi
         ;;
 esac
@@ -34,5 +21,27 @@ SECRET=`cat /mastodon/private/secret`
 export PAPERCLIP_SECRET=$SECRET
 export SECRET_KEY_BASE=$SECRET
 export OTP_SECRET=$SECRET
+
+case "$*" in
+    *rails*server*)
+        if [ ! -e /mastodon/private/migrated-1 ]; then
+            echo "initializing/updating Mastodon"
+            sleep 5
+
+            VERSION_BAK=$VERSION
+            unset VERSION
+            rake db:migrate
+            export VERSION=$VERSION_BAK
+            
+            rake secret > /mastodon/private/secret
+            
+            rake assets:precompile
+
+            rails runner /mastodon/create_admin.rb
+            
+            touch /mastodon/private/migrated-1
+        fi
+        ;;
+esac
 
 exec "$@"
