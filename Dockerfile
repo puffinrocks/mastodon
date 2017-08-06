f8fe17693f9217eb7acc3dd0a1dad9add73bbd12
Jarek Lipski <loomchild@loomchild.net>
2017-08-06 23:02:25 +0200

    Update version to 1.5.0, change user to mastodon

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM gargron/mastodon:v1.3.3
+FROM gargron/mastodon:v1.5.0
 
-ENV VERSION 1.3.3
+ENV VERSION 1.5.0
 
 COPY create_admin.rb /mastodon/create_admin.rb
 
--- create_admin.rb
+++ create_admin.rb
@@ -1,5 +1,5 @@
-account = Account.create!(username: 'admin')
-user = User.create!(email: 'admin@localhost', password: 'password', admin: true, account: account)
+account = Account.create!(username: 'mastodon')
+user = User.create!(email: 'mastodon@localhost', password: 'password', admin: true, account: account)
 user.confirm
 account.save!
 user.save!
